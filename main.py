22b80718250b8b1d386cc4ee821ef28adc878094
RealRahan <103349690+RealRahan@users.noreply.github.com>
2022-07-17 15:49:16 +0300

    update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ client.remove_command("help")
 @client.event
 async def on_ready():
  print(f"تم تشغيل بوت {client.user.name} بنجاح")
- await client.change_presence(activity=discord.Game(name="إياس عم دريكس")
+ await client.change_presence(activity=discord.Game(name="إياس عم دريكس"))
  azkar.start()
 
 @tasks.loop(seconds = 900) # repeat after every 15 mins
